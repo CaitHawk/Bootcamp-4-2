e8d2949445fb84790d5440cfd324626e4250fcd2
Caitlin Neathawk <neathawkcaitlin@gmail.com>
2022-01-22 12:35:31 -0800

    Week 1 Workshop assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -24,7 +24,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
     .all((req, res, next) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/pain');
+        res.setHeader('Content-Type', 'text/plain');
         next();
     })
     .get((req, res) => {
